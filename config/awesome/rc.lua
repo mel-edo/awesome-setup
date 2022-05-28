6918f9e83e0a7d28d18e2c3c9d19a1a751b240f8
MeledoJames <90543784+MeledoJames@users.noreply.github.com>
2022-05-28 22:35:59 +0530

    awesome config updated

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -555,10 +555,20 @@ client.connect_signal("request::titlebars", function(c)
 end)
 
 -- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal("mouse::enter", function(c)
-    c:emit_signal("request::activate", "mouse_enter", {raise = false})
-end)
+--client.connect_signal("mouse::enter", function(c)
+--    c:emit_signal("request::activate", "mouse_enter", {raise = false})
+--end)
 
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+--client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+--client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- Gaps
+
+beautiful.useless_gap = 5
+
+-- Autostart
+
+awful.spawn.with_shell("picom")
+
+
