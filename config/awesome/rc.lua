c5e8d885678811209ca4f3d86d77f74f2afbf88b
MeledoJames <meledo@duck.com>
2022-07-15 07:53:33 +0530

    switched to alacritty, firefox config, minor changes in polybar and awesomerc

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -48,7 +48,7 @@ end
 beautiful.init("/home/meledo/.config/awesome/theme-def.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "kitty"
+terminal = "alacritty"
 editor = os.getenv("EDITOR") or "nvim"
 editor_cmd = terminal .. " -e " .. editor
 
